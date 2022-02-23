b932ef16125d597c3f6508816afc618e27228154
OlgaKireenko <80949699+OlgaKireenko@users.noreply.github.com>
2022-02-23 19:00:51 +0500

    Update CashbackHackServiceTest.java
    expected_modified

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -34,7 +34,7 @@ public void shouldCountCashback1(){
         CashbackHackService service = new CashbackHackService();
 
 
-        int expected = 1000;
+        int expected = 0;
         int actual = service.remain(1000);
         assertEquals(actual, expected);
     }
